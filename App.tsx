3712fc6796c5f91df92fddd58034ee3464983551
sidhanta33crs <sidhanta.rout@33crores.com>
2025-06-08 11:54:42 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+import { WebView } from 'react-native-webview'
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://esmsv2.scriptlab.in/site/login' }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  webview: {
+    flex: 1,
+  },
+})
